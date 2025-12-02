f5ffd1485234ad8bcd759b719106ee8f1f408ea2
cozmadacian4@gmail.com <cozmadacian4@gmail.com>
2025-12-02 23:53:06 +0200

    da

--- AppState.cs
+++ AppState.cs
@@ -1,15 +1,29 @@
 ﻿using mlDataSet;
-using Models;
+using DataInterpretation;
+using System;
+using System.Collections.Generic;
 
 namespace machine_learnintg.Services
 {
+    // 1. Define this class here so it is accessible by both the Service and the Pages
+    public class ColumnConfig
+    {
+        public string Name { get; set; }
+        public object PreviewValue { get; set; }
+        public bool IsFeature { get; set; } = true;
+        public bool IsLabel { get; set; } = false;
+    }
+
     public class AppState
     {
-        // The processed dataset ready for training
+        // 2. The Final Dataset (Ready for training)
         public MLDataSet CurrentDataSet { get; private set; }
 
-        // The raw dataframe (optional, if you want to keep it)
-        public DataInterpretation.DataFrame RawDataFrame { get; private set; }
+        // 3. The Raw Data (Persists the uploaded CSV)
+        public DataFrame RawDataFrame { get; private set; }
+
+        // 4. The Configuration State (Persists your checkboxes/radio buttons)
+        public List<ColumnConfig> ImportConfigurations { get; private set; }
 
         public event Action OnChange;
 
@@ -19,9 +33,11 @@ namespace machine_learnintg.Services
             NotifyStateChanged();
         }
 
-        public void SetDataFrame(DataInterpretation.DataFrame df)
+        // 5. New Method: Save the raw import state
+        public void SaveImportState(DataFrame rawFrame, List<ColumnConfig> configs)
         {
-            RawDataFrame = df;
+            RawDataFrame = rawFrame;
+            ImportConfigurations = configs;
             NotifyStateChanged();
         }
 
